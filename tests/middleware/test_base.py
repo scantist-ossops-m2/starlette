dcfff2f07756c5b6cd0f06b8baf73ad2f9873c11
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2024-06-13 15:09:26 -0500

    fmt

--- tests/middleware/test_base.py
+++ tests/middleware/test_base.py
@@ -1038,7 +1038,6 @@ def test_pr_1519_comment_1236166180_example() -> None:
     assert bodies == [b"Hello, World!-foo"]
 
 
-
 @pytest.mark.anyio
 async def test_multiple_middlewares_stacked() -> None:
     class DummyMiddleware(BaseHTTPMiddleware):
